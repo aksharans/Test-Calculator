648a5e1362109ac01d97826d1e8607894419b6b5
aksharans <aksharan@gmail.com>
2020-01-07 17:44:19 -0800

    UI and color improvements

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,31 +2,58 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textview;
+
+    Button add_btn;
+    Button sub_btn;
+    Button mul_btn;
+    Button div_btn;
+
+    TextView display;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button add_btn = findViewById(R.id.add_btn);
-        Button sub_btn = findViewById(R.id.sub_btn);
-        Button mul_btn = findViewById(R.id.mul_btn);
-        Button div_btn = findViewById(R.id.div_btn);
 
+        textview = findViewById(R.id.txtview);
 
+        add_btn = findViewById(R.id.add_btn);
+        sub_btn = findViewById(R.id.sub_btn);
+        mul_btn = findViewById(R.id.mul_btn);
+        div_btn = findViewById(R.id.div_btn);
 
+        display = findViewById(R.id.display);
+
+        ObjectAnimator animate = ObjectAnimator.ofInt(display, "TextColor", Color.argb(200,180,57,52),
+                Color.argb(200,248,116,68), Color.argb(200,163,209,0),
+                Color.argb(200,0,247,129), Color.argb(200,0,145,243),
+                Color.argb(200,152,8,255),Color.argb(200,244,0,153)
+        );
+        animate.setEvaluator(new ArgbEvaluator());
+        animate.setDuration(6000);
+        animate.setRepeatMode(ValueAnimator.REVERSE);
+        animate.setRepeatCount(ValueAnimator.INFINITE);
+        animate.start();
 
         add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textview = findViewById(R.id.txtview);
                 EditText num1 = findViewById(R.id.num1);
                 EditText num2 = findViewById(R.id.num2);
 
@@ -38,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 float number2 = Float.parseFloat(num2.getText().toString());
                 float ans = number1 + number2;
                 String ansStr = ans + "";
-                textview.setText(ansStr);
+                set_Text(ansStr, add_btn);
 
                 num1.getText().clear();
                 num2.getText().clear();
@@ -52,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
         sub_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textview = findViewById(R.id.txtview);
                 EditText num1 = findViewById(R.id.num1);
                 EditText num2 = findViewById(R.id.num2);
 
@@ -64,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
                 float number2 = Float.parseFloat(num2.getText().toString());
                 float ans = number1 - number2;
                 String ansStr = ans + "";
-                textview.setText(ansStr);
+                set_Text(ansStr, sub_btn);
 
                 num1.getText().clear();
                 num2.getText().clear();
@@ -78,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
         mul_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textview = findViewById(R.id.txtview);
                 EditText num1 = findViewById(R.id.num1);
                 EditText num2 = findViewById(R.id.num2);
 
@@ -90,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
                 float number2 = Float.parseFloat(num2.getText().toString());
                 float ans = number1 * number2;
                 String ansStr = ans + "";
-                textview.setText(ansStr);
+                set_Text(ansStr, mul_btn);
 
                 num1.getText().clear();
                 num2.getText().clear();
@@ -104,7 +129,6 @@ public class MainActivity extends AppCompatActivity {
         div_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textview = findViewById(R.id.txtview);
                 EditText num1 = findViewById(R.id.num1);
                 EditText num2 = findViewById(R.id.num2);
 
@@ -116,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
                 float number2 = Float.parseFloat(num2.getText().toString());
                 float ans = number1 / number2;
                 String ansStr = ans + "";
-                textview.setText(ansStr);
+                set_Text(ansStr, div_btn);
 
                 num1.getText().clear();
                 num2.getText().clear();
@@ -143,6 +167,22 @@ public class MainActivity extends AppCompatActivity {
 
     public int division(int num1, int num2) {
         return num1 / num2;
+    }
+
+    public void set_Text (String ans, Button b){
+
+
+        ObjectAnimator anim = ObjectAnimator.ofInt(textview, "TextColor", Color.argb(10,255,255,255), Color.argb(180,11,40,98), Color.argb(255,11,40,98));
+        anim.setDuration(1000);
+        anim.setEvaluator(new ArgbEvaluator());
+        anim.start();
+        textview.setText(ans);
+
+        ObjectAnimator anim2 = ObjectAnimator.ofInt(b, "BackgroundColor", Color.LTGRAY, Color.argb(180,11,40,98),Color.LTGRAY );
+        anim2.setDuration(1000);
+        anim2.setEvaluator(new ArgbEvaluator());
+        anim2.start();
+
     }
 
 
